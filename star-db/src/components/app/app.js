9ab8575dea9b3318912f7ee2c0c3fa1a975e8480
i.m.a.1988@gmail.com <i.m.a.1988@gmail.com>
2020-01-23 19:42:42 +0300

    upd for routes

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -63,7 +63,9 @@ export default class App extends Component {
 
               <RandomPlanet />
               <Route path="/" exact render={() => <h2>Welcome</h2>} />
-              <Route path="/people" component={PeoplePage} />
+              <Route path="/people" exact render={() => <h2>People</h2>} />
+              <Route path="/people/:id" component={PeoplePage} />
+              
               <Route path="/planets" component={PlanetsPage} />
               <Route path="/starships" exact component={StarshipsPage} />
               <Route path="/starships/:id" exact
